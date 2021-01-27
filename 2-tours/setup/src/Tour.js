c7b5e6cc8f3d5af0d8f04a503c1c72b6236e0253
Stephanie Green <steph.green44@gmail.com>
2021-01-27 15:48:33 -0700

    add tours

--- 2-tours/setup/src/Tour.js
+++ 2-tours/setup/src/Tour.js
@@ -1,7 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, image, price, info, name }) => {
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4>${price}</h4>
+        </div>
+        <p>{info}</p>
+        <button className="delete-btn">not interested</button>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
